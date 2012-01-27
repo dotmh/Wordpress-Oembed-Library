f35c2e0e99b3c30c4e55202823e94591d0e5d213
Martin Haynes <martin@dotmh.com>
2012-01-27 10:23:02 +0000

    Add in safe guard to stop the class been declared twice , as multi class declarations is not support by PHP

--- embed.php
+++ embed.php
@@ -38,189 +38,190 @@
 
 	@return null
 **/
-
-class dotmh_embed
-{
-	protected $endpoint;
-
-	protected $data;
-
-	protected $url;
-
-	protected $height;
-
-	protected $width;
-
-	protected $other = array();
-
-	private $required = array('url');
-
-	public function __construct( $params = null , $autoload = FALSE )
+if ( !class_exists('dotmh_embed')) {
+	class dotmh_embed
 	{
-		if ( !is_null($params) ) {
-			foreach( $params as $param => $value ) {
-				$this->__set($param , $value);
+		protected $endpoint;
+
+		protected $data;
+
+		protected $url;
+
+		protected $height;
+
+		protected $width;
+
+		protected $other = array();
+
+		private $required = array('url');
+
+		public function __construct( $params = null , $autoload = FALSE )
+		{
+			if ( !is_null($params) ) {
+				foreach( $params as $param => $value ) {
+					$this->__set($param , $value);
+				}
+			}
+
+			if ( $autoload ) {
+				$this->load();
 			}
 		}
 
-		if ( $autoload ) {
-			$this->load();
-		}
-	}
+		/**
+			Load the Oembed data from the endpoint using the request vars
 
-	/**
-		Load the Oembed data from the endpoint using the request vars
+			It will throw an exception on error , so that you can manage errors how you see fit the error will ether include 
+			$code int The http error code i.e. 404.
+			$message string the http message i.e. page not found
 
-		It will throw an exception on error , so that you can manage errors how you see fit the error will ether include 
-		$code int The http error code i.e. 404.
-		$message string the http message i.e. page not found
+			or the wordpress error information
 
-		or the wordpress error information
+			@param null
+			@return null
+		**/
+		public function load() 
+		{
+			$response = wp_remote_get($this->build_url());
 
-		@param null
-		@return null
-	**/
-	public function load() 
-	{
-		$response = wp_remote_get($this->build_url());
+			if ( is_wp_error($response) ) {
+				throw new Exception($response->get_error_message() , $response->get_error_code());
+			}
+			else {
+				if ( $response['response']['code'] != 200 ) {
+					/*
+						 Stop : The response from the server was invalid
+						 --
+						 Throw an exception so that the libraries user can handle it as they need
+						 
+						 Code 	:	is set to the http response code
+						 Message: 	is the http message
+					*/
+					throw new Exception($response['response']['message'] , $response['response']['code']);
+				}
 
-		if ( is_wp_error($response) ) {
-			throw new Exception($response->get_error_message() , $response->get_error_code());
-		}
-		else {
-			if ( $response['response']['code'] != 200 ) {
-				/*
-					 Stop : The response from the server was invalid
-					 --
-					 Throw an exception so that the libraries user can handle it as they need
-					 
-					 Code 	:	is set to the http response code
-					 Message: 	is the http message
-				*/
-				throw new Exception($response['response']['message'] , $response['response']['code']);
+				// All Good we have something from the server
+
+				$data = json_decode( $response['body'] , true);
+
+				// Check something came back that we can use after we smashed it through the decoder
+				if ( is_null($data) || $data === FALSE || count($data) < 1 ) {
+					throw new Exception("This is not the response you are looking for", 2);
+				}
+
+				// All is well , the world is a better place 
+				$this->data = $data;
 			}
 
-			// All Good we have something from the server
+		}
+		/**
+			Builds the request url , currently the format is set to json , this is due to the way the response is processed. 
 
-			$data = json_decode( $response['body'] , true);
+			@todo support the format var
 
-			// Check something came back that we can use after we smashed it through the decoder
-			if ( is_null($data) || $data === FALSE || count($data) < 1 ) {
-				throw new Exception("This is not the response you are looking for", 2);
-			}
+			@params null
+			@return string the complete request
+		**/
+		public function build_url() {
 
-			// All is well , the world is a better place 
-			$this->data = $data;
+			$this->is_valid();
+
+			$parts = array();
+			array_push($parts, $this->httpify($this->endpoint));
+			array_push($parts, '?');
+			array_push($parts, 'url='.urlencode($this->httpify($this->url)).'&');
+
+			if ( !empty($this->maxheight) ) array_push($parts, 'maxheight='.$this->maxheight.'&');
+			if ( !empty($this->maxwidth) )array_push($parts, 'maxwidth='.$this->maxwidth.'&');
+
+			array_push($parts, $this->build_other_params());
+			array_push($parts, 'format=json');
+
+			return implode('', $parts);
 		}
 
-	}
-	/**
-		Builds the request url , currently the format is set to json , this is due to the way the response is processed. 
+		/**
+			Check that the request will be valid and all required fields are complete 
 
-		@todo support the format var
-
-		@params null
-		@return string the complete request
-	**/
-	public function build_url() {
-
-		$this->is_valid();
-
-		$parts = array();
-		array_push($parts, $this->httpify($this->endpoint));
-		array_push($parts, '?');
-		array_push($parts, 'url='.urlencode($this->httpify($this->url)).'&');
-
-		if ( !empty($this->maxheight) ) array_push($parts, 'maxheight='.$this->maxheight.'&');
-		if ( !empty($this->maxwidth) )array_push($parts, 'maxwidth='.$this->maxwidth.'&');
-
-		array_push($parts, $this->build_other_params());
-		array_push($parts, 'format=json');
-
-		return implode('', $parts);
-	}
-
-	/**
-		Check that the request will be valid and all required fields are complete 
-
-		Throws exception if the request is invalid.
-	**/
-	public function is_valid() {
-		foreach ( $this->required as $param ) {
-			if ( empty($this->{$param} ) ) {
-				throw new Exception("{$param} is a required field by the oembed specification here http://www.oembed.com/");
-			}
-		}
-	}
-
-	/**
-		Outputs the response as an assoc array
-
-		@return array The response data
-	**/
-	public function __toArray() {
-		return $this->data;
-	}
-
-	/**
-		Add the protocol on the url if its not present , this also checks to see if the request was made using ssl and if so adds https rather than http
-
-		@returns string the url + the protocol
-	**/
-	protected function httpify( $url ) {
-
-		if ( ! preg_match('/^http|https/', $url) ) {
-			$prefix = empty($_SERVER['HTTPS'] ) ? 'http://' : 'https://';
-			return $prefix.$url;
-		}
-		return $url;
-	}
-
-	/**
-		Takes any other parameters you want to pass in the request and converts them to a query string 
-
-		@return string the completed query string.
-	**/
-	protected function build_other_params() {
-
-		$string = '';
-
-		if ( count($this->other) > 0 ) {
-			$loop_counter = 0;
-			foreach ($this->other as $key => $value) {
-				$string .= ($loop_counter < 0 ? '&' : '').$key.'='.$value;
-				$loop_counter++;
+			Throws exception if the request is invalid.
+		**/
+		public function is_valid() {
+			foreach ( $this->required as $param ) {
+				if ( empty($this->{$param} ) ) {
+					throw new Exception("{$param} is a required field by the oembed specification here http://www.oembed.com/");
+				}
 			}
 		}
 
-		return $string;
-	}
+		/**
+			Outputs the response as an assoc array
 
-	/**
-		Magic get Function see 
-		@link http://www.php.net/manual/en/language.oop5.overloading.php#object.get
-	**/
-	public function __get($name) {
-		
-		$name = strtolower(preg_replace('/([A-Z])/', ':$1', $name));
-
-		if ( array_key_exists($name, $this->data ) ) {
-			return $this->data[$name];
+			@return array The response data
+		**/
+		public function __toArray() {
+			return $this->data;
 		}
 
-		return FALSE;
-	}
+		/**
+			Add the protocol on the url if its not present , this also checks to see if the request was made using ssl and if so adds https rather than http
 
-	/**
-		Magic set Function see 
-		@link http://www.php.net/manual/en/language.oop5.overloading.php#object.set
-	**/
-	public function __set($name , $value ) {
-		if ( property_exists($this, $name) ) {
-			$this->{$name} = $value;
+			@returns string the url + the protocol
+		**/
+		protected function httpify( $url ) {
+
+			if ( ! preg_match('/^http|https/', $url) ) {
+				$prefix = empty($_SERVER['HTTPS'] ) ? 'http://' : 'https://';
+				return $prefix.$url;
+			}
+			return $url;
 		}
-		else {
-			$other[$name] = $value;
+
+		/**
+			Takes any other parameters you want to pass in the request and converts them to a query string 
+
+			@return string the completed query string.
+		**/
+		protected function build_other_params() {
+
+			$string = '';
+
+			if ( count($this->other) > 0 ) {
+				$loop_counter = 0;
+				foreach ($this->other as $key => $value) {
+					$string .= ($loop_counter < 0 ? '&' : '').$key.'='.$value;
+					$loop_counter++;
+				}
+			}
+
+			return $string;
+		}
+
+		/**
+			Magic get Function see 
+			@link http://www.php.net/manual/en/language.oop5.overloading.php#object.get
+		**/
+		public function __get($name) {
+			
+			$name = strtolower(preg_replace('/([A-Z])/', ':$1', $name));
+
+			if ( array_key_exists($name, $this->data ) ) {
+				return $this->data[$name];
+			}
+
+			return FALSE;
+		}
+
+		/**
+			Magic set Function see 
+			@link http://www.php.net/manual/en/language.oop5.overloading.php#object.set
+		**/
+		public function __set($name , $value ) {
+			if ( property_exists($this, $name) ) {
+				$this->{$name} = $value;
+			}
+			else {
+				$other[$name] = $value;
+			}
 		}
 	}
 }
